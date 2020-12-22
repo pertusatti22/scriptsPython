5c662ba90baae5942b87f81c42f381a43f1193f5
pertusatti22 <pertusatti22@gmail.com>
2020-12-22 13:27:21 -0300

    Ajuste de Pastas - Conclusão mundo 1 - Fundamentos

--- mundo1-fundamentos/Aulas/aula7_operadoresAritmeticos.py
+++ mundo1-fundamentos/Aulas/aula7_operadoresAritmeticos.py
@@ -12,3 +12,5 @@ divisaoInteira = numero1 // numero2
 divisaoModulo = numero1 % numero2
 
 print('A soma é {},\na subtração é {},\no produto é {},\na divisão é {},\na potenciação é {},\na divisão inteira é {}\ne o resto da divisão é {}'.format(soma, subtracao, multiplicacao, divisao, potenciacao, divisaoInteira, divisaoModulo))
+teste = 3*5+4**2
+print(teste)
