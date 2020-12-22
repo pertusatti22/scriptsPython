993597b44d9c41f5b03a6c4d114b47bb756d0187
pertusatti22 <pertusatti22@gmail.com>
2020-12-22 15:16:45 -0300

    ajuste de diferenças

--- mundo2-repetição,conceitos,usabilidade/Desafios/ex036_emprestimoImobiliario.py
+++ mundo2-repetição,conceitos,usabilidade/Desafios/ex036_emprestimoImobiliario.py
@@ -1,10 +1,8 @@
-preco = float(input('Qual o valor da casa: '))
-renda = float(input('Qual é o valor do seu salário: '))
-prazo = float(input('Qual o prazo (anos) de pagamento almejado: ')
-
-#calcular prestação mensal, que não pode exceder 30% do salário, caso exceda, empréstimo negado.
-
-if mensalidade < renda*0.3:
-    print ('Empréstimo aprovado, sua prestação mensal ficou em R${:.2f}'.format(mensalidade)
+valorCasa = float(input('Informe o preço da casa: '))
+salario = float(input('Informe sua renda mensal: ')) 
+prazoPagamento = float(input('Informe em quantos anos quer pagar a casa: '))
+mensalidade = valorCasa/(prazoPagamento*12)
+if mensalidade <= salario*0.30:
+    print('Parabéns! Sua parcela é de R${:.2f}'.format(mensalidade))
 else:
-    print ('Seu empréstimo foi Negado!')
+    print('Dinheiro negado!')
